fe5482d54c817dca4d2544c9dd5fb87f2c46473f
Merve <mervehosol9@gmail.com>
2023-01-25 01:59:12 +0300

    update

--- src/main/java/com/kodlamaio/common/CommonApplication.java
+++ src/main/java/com/kodlamaio/common/CommonApplication.java
@@ -1,5 +1,7 @@
 package com.kodlamaio.common;
 
+import com.kodlamaio.common.utilities.mapping.ModelMapperManager;
+import com.kodlamaio.common.utilities.mapping.ModelMapperService;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +10,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CommonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommonApplication.class, args);
-	}
-	@Bean
-	public ModelMapper getModelMapper() {
-		return new ModelMapper();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public ModelMapperService getModelMapperService(ModelMapper mapper) {
+        return new ModelMapperManager(mapper);
+    }
 
 }
